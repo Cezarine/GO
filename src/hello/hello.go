d9cf2f2010a787ba4d358554acff60d478fabfd1
Cezarine <gcezarines@gmail.com>
2023-03-26 18:42:59 -0300

    Final Modulo 03 Go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Olá Mundo. Eu sou Guilherme")
+	Introduction()
+	var vComando = displayMenu()
+	switch vComando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Exiting")
+		os.Exit(0)
+	default:
+		fmt.Println("Comando não encontrado")
+	}
+
+}
+
+func Introduction() {
+	const vVersao float32 = 1.1
+	var vNome string
+	fmt.Println("Qual seu nome?")
+	fmt.Scan(&vNome)
+	fmt.Println("Olá, Sr/Sra.", vNome)
+	fmt.Println("Este programa esta na versao", vVersao)
+}
+
+func displayMenu() int {
+	var vComando int
+	fmt.Println("1- Iniciar Monitoriamento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair")
+	fmt.Scan(&vComando)
+
+	return vComando
 }
